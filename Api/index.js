be6fca798aa0f4afa9f3149a84e96048cb542529
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-04-08 17:57:38 -0500

    Agregar Index Api

---
+++ Api/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+//Invocar las Rutas de la Carpeta Routers
+const routers = require('./routers/index');
+app.use(routers);
+
+
+// Iniciando el Api 
+const port = 3000
+app.listen(port, () => {
+    console.log(`API node: http://localhost:${port}/`);
+});
