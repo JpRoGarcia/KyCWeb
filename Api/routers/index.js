39d7c8519bfdae471676cfc63c9c02fd4be0a8d6
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-04-09 17:45:53 -0500

    Agragr Rutas

--- Api/routers/index.js
+++ Api/routers/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const userController = require("../controllers/emprendedor.controller")
+//const userController = require("../controllers/emprendedor.controller")
 
 
-/*router.get('/', (req, res) => {
-  res.render('index.html', {title: "Wenas Perro"});
+router.get('/', (req, res) => {
+  res.render('index.html', {title: "Inicio"});
 });
 
-router.get('/Contact', (req, res) => {
-  res.render('contact.html', {title: "Contacto COntactudo"});
-});*/
+router.get('/Formalizar', (req, res) => {
+  res.render('formalizar.html', {title: "Formalizar"});
+});
 
-router.get("/usuarios", userController.getUsers);
+router.get('/Videos', (req, res) => {
+  res.render('videos.html', {title: "Videos"});
+});
+
+router.get('/Guia', (req, res) => {
+  res.render('guia.html', {title: "Guia"});
+});
+
+router.get('/Contacto', (req, res) => {
+  res.render('contact.html', {title: "Contacto"});
+});
+
+
+//router.get("/usuarios", userController.getUsers);
 
 module.exports = router; 
