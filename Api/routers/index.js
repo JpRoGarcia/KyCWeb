1d052abc5172b857fc78c9032b6da0b57a179c06
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-04-09 16:03:23 -0500

    html

--- Api/routers/index.js
+++ Api/routers/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {title: "Wenas Perro"});
+  res.render('index.html', {title: "Wenas Perro"});
 });
 
 router.get('/Contact', (req, res) => {
-  res.render('contact', {title: "Contacto COntactudo"});
+  res.render('contact.html', {title: "Contacto COntactudo"});
 });
 
 module.exports = router; 
