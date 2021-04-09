42a04e9c7e7729646a3ed6535259a8418f047039
pea0715 <pabloespinosarias11c@gmail.com>
2021-04-09 17:42:28 -0500

    subiendo routers

--- Api/routers/index.js
+++ Api/routers/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const userController = require("../controllers/emprendedor.controller")
 
-router.get('/', (req, res) => {
+
+/*router.get('/', (req, res) => {
   res.render('index.html', {title: "Wenas Perro"});
 });
 
 router.get('/Contact', (req, res) => {
   res.render('contact.html', {title: "Contacto COntactudo"});
-});
+});*/
+
+router.get("/usuarios", userController.getUsers);
 
 module.exports = router; 
