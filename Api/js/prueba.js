46db8541400fc93cdb0a3f6d67308e8ff9b20f4f
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-04-10 23:38:32 -0500

    9

---
+++ Api/js/prueba.js
@@ -0,0 +1,6 @@
+function enviarAlerta(){
+    var cedula = document.getElementById("cedulaR").value;
+    var name = document.getElementById("NombreR").value;
+    console.log(name + " " + cedula);
+}
+
