5ba45a2b3727ac3b7df401dba2551f8ef3414fcc
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-04-11 20:12:52 -0500

    13

--- Api/routers/index.js
+++ Api/routers/index.js
@@ -1,7 +1,13 @@
+const bcrypt = require("bcrypt");
 const express = require('express');
 const router = express.Router();
+const App = express();
 const userController = require("../controllers/emprendedor.controller")
-const assesoresController = require("../controllers/assesores.controller")
+const assesoresController = require("../controllers/assesores.controller");
+const { Pool, Query } = require("pg");
+const { query } = require("express");
+router.use(express.json());  
+router.use(express.urlencoded()); 
 
 router.get('/', (req, res) => {
   res.render('index.html', {title: "Inicio"});
@@ -27,7 +33,7 @@ router.get('/InicioSesion', (req, res) => {
   res.render('iniciosesion.html', {title: "Inicio Sesion"});
 });
 
-router.get('/Registro', (req, res) => {
+router.get('/Emprendedor/Registro', (req, res) => {
   res.render('registro.html', {title: "Registro"});
 });
 
@@ -35,64 +41,42 @@ router.get('/Entro', (req, res) => {
   res.render('entro.html', {title: "Inicio Sesion Exitoso", user: "Pedro"});
 });
 
-router.post("/Registro", (req, res) => {
+router.post("/Emprendedor/Registro", async (req, res) => {
   let { id, name, lastname, email, password1, password2, movil, phone } = req.body;
 
-  console.log({id,
+  let errors = [];
+  
+  console.log({
+    id,
     name,
     lastname,
     email,
     password1,
     password2,
     movil,
-    phone});
-
-    let errors = []
+    phone
+  });
 
     if(!name || !lastname || !email || !password1 || !password2 || !movil || !phone){
-      errors.push({ mensaje: "Viejo Bruto le quedo un espacion Vacio" });
+      errors.push({ message: "Viejo Bruto le quedo un espacion Vacio" });
     }
 
-    if(password1.lenght < 6){
-      errors.push({ mensaje: "Viejo Bruto la Contraseña esta muy corta" });
+    if (password1.length < 6) {
+      errors.push({ message: "Viejo Bruto la Contraseña esta muy corta" });
     }
 
-    if(password1 != password2){
-      errors.push({ mensaje: "Viejo Bruto las contraseña no coinsiden" });
+    if (password1 !== password2) {
+      errors.push({ message: "Viejo Bruto las contraseña no coinsiden" });
     }
 
     if(errors.length > 0){
-      res.render("Registro", {errors})
+      res.render('/Emprendedor/Registro', {errors})
     } else {
-      let ContraseñaEncriptada = bcrypt.hash(password1)
+      let ContraseñaEncriptada = await bcrypt.hash(password1, 10)
+      console.log(ContraseñaEncriptada);
+
     }
+
 });
 
-router.get("/usuarios", userController.getUsers);
-
-/*
-//Ruta para la creacion de emprendedor
-router.post("/usuarios", userController.saveUsers);
-
-//Ruta para la creacion de assesores
-router.post("/assesores", assesoresController.saveassesores);
-
-//Ruta para eliminar un emprendedor
-router.delete("/usuarios/:cedula", userController.deleteUsers);
-
-//Ruta para eliminar un assesores
-router.delete("/assesores/:cedula", assesoresController.deleteassesores);
-
-//Ruta para ver Todos los emprendedor
-
-
-//Ruta para ver Todos los assesor
-router.get("/assesores", assesoresController.getassesores);
-
-//Ruta para Modificar un emprendedor
-router.put("/usuarios/:cedula", userController.updateUsers);
-
-//Ruta para Modificar un assesor
-router.put("/assesores/:cedula", assesoresController.updateassesores);
-*/
 module.exports = router; 
